e26a3e7eb978b1ae5fe96fab1047ddb4723786a4
yash gehlot <yashgehlot1233@gmail.com>
2025-12-22 23:20:24 +0530

    added a global error handler

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import sequelize from "./src/config/db.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
-
-// Routes imports
 import authRoutes from "./src/routes/authRoutes.js";
 import userRoutes from "./src/routes/userRoutes.js";
 import staffRoutes from "./src/routes/staffRoutes.js";
@@ -23,14 +19,10 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ quiet: true });
 const app = express();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "src", "public")));
-
 app.use(express.static(path.join(__dirname, "src", "views")));
 
 // API Routes
@@ -42,43 +34,13 @@ app.use("/payments", paymentRoutes);
 app.use("/reviews", reviewRoutes);
 app.use("/admin", adminRoutes);
 
+app.use(errorHandler);
+
 // View Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "src", "views", "index.html"));
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "register.html"));
-});
-
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "dashboard.html"));
-});
-
-app.get("/services", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "services.html"));
-});
-
-app.get("/booking", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "booking.html"));
-});
-
-app.get("/appointments", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "appointments.html"));
-});
-
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "admin.html"));
-});
-
-// Error handling middleware (must be last)
-app.use(errorHandler);
-
-// Database connection and server start
 sequelize
   .sync()
   .then(() => {
